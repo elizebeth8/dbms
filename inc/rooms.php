f9549574cf1cec8f570d54abc2bc5392c9037b1f
elizebeth8 <elizebethshaji8@gmail.com>
2023-11-21 00:38:59 +0530

    r

---
+++ inc/rooms.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>dbms</title>
+</head>
+<body class="bg-light">
+   
+    <div class="my-5 px-4">
+        <h2 class="fw-bold h-font text-center">OUR ROOMS</h2>
+        <div class="h-line bg-dark"></div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3">
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
